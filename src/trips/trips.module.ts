ec005d0012f29e30f8153bb3d7c1f9a0498d382c
ldruley <logan.druley@gmail.com>
2025-04-13 21:36:23 -0700

    Forgot to update our module files for our repositories

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TripsController } from './trips.controller';
 import { TripsService } from './trips.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Trip} from "./entities/trip.entity";
+import {TripsRepository} from "./repository/trips.repository";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Trip])],
   controllers: [TripsController],
-  providers: [TripsService]
+  providers: [TripsService, TripsRepository],
+  exports: [TripsService, TripsRepository]
 })
 export class TripsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {UsersRepository} from "./repository/users.repository";
+import {User} from "./entities/user.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService, UsersRepository]
 })
 export class UsersModule {}
